813f49e56e03ca3ea3716c21347017d3c035ec6b
rqrqrqrq <rtdfh@gmail.com>
2017-07-07 11:44:55 +0300

    update deps and example

--- src/example.js
+++ src/example.js
@@ -1,1 +1,5 @@
 export default (a: number, b: number): number => a + b;
+
+export function mapThroughObject<T>(f: any => T, o: Object): T[] {
+  return Object.values(o).map(f);
+}
--- src/example.test.js
+++ src/example.test.js
@@ -1,5 +1,13 @@
-import sum from './example';
+import sum, { mapThroughObject } from './example';
 
 test('my testcase', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('another testcase', () => {
+  expect(mapThroughObject(v => `1${v}`, { a: 1, b: 2, c: 3 })).toEqual([
+    '11',
+    '12',
+    '13',
+  ]);
 });
